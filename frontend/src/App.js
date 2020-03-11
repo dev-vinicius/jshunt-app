9e245055d40c779b788d21e48fb01326b82c3fe8
Vinicius S Sanchez <viniciusanchez@gmail.com>
2020-03-10 21:28:41 -0300

    Importação do backend e frontend
    Importação dos arquivos do backend e frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Routes from './routes';
+import './styles.css';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+    return (
+        <header id="main-header">JsHunt</header>
+    );
+}
+
+export default Header;
